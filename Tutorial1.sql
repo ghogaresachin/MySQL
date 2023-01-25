d1e3bdd48f497ac3353ef7c78c1620f73bd6cfe9
Sachin Ghogare <93420197+ghogaresachin@users.noreply.github.com>
2023-01-25 20:52:58 +0530

    Add files via upload

---
+++ Tutorial1.sql
@@ -0,0 +1,24 @@
+create table Statistics(Nam  varchar(10),marks integer,percentage float);
+
+
+#Session First.
+#1. Create Databases. syntax( create database Nameofdatabase)
+#2. Drop Databases.   syntax( drop database Nameofdatabase)
+#3. Create Tables.    syntax( create table NameofTable(id integer,first_name varchar(20),last_name varchar(20)))
+#4. Insert Into Table syntax( insert into NameofTable(id,first_name,last_name) values(1,'Chetan','Salunke')
+#5. Drop Table.
+
+
+
+
+show tables;
+select * from Statistics;
+insert into Statistics(Nam,marks,percentage) values('Chetan',442,72.33);
+insert into Statistics(Nam,marks,percentage) values('Sachin',350,59.34);
+insert into Statistics(Nam,marks,percentage) values('Pradip 50K',400,69.5);
+drop table Statistics;
+show tables;
+show databases
+
+
+#
